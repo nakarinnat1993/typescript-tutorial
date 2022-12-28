69f2e3c6cdbd3277b452f17233be6fadd75b1999
Nakarin Jaiseengam <nakarinnat1993@gmail.com>
2022-12-28 14:00:09 +0700

    DataType

---
+++ 03DataType.ts
@@ -0,0 +1,8 @@
+const myName: string = "Nut";
+const myNumber: number = 0;
+const myArrayNumber: number[] = [1, 2, 3];
+const myArrayAny: any[] = [1, 2, 3, "Hello world"];
+const myTuple: [number, string] = [1, "Hello world"];
+const myTupleArray: [number, string][] = [[1, "Hello world"], [2, "Hello world"], [3, "Hello world"]];
+const myObject: object = {id:1, name:"Hello world"};
+const myTupleArrayObject: {id:number, name:string}[] = [{id:1, name:"Hello world"}, {id:2, name:"Hello world"}, {id:3, name:"Hello world"}];
